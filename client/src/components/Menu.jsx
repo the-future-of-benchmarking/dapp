aec86244a24bb9842d9a42637a687851787ff962
Lukas Fruntke <lukas@fruntke.tech>
2021-07-16 20:11:52 +0200

    Add static hosting ability

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -13,19 +13,19 @@ export class Menu extends Component{
     render(){
         const items = [
             {
-                label: 'Home', icon: 'pi pi-fw pi-home', command:()=>{ this.navigateToPage('/') }, 
+                label: 'Home', icon: 'pi pi-fw pi-home', command:()=>{ this.navigateToPage(process.env.PUBLIC_URL+'/') }, 
             },
             {
-                label: 'An einem Benchmark teilnehmen', icon: 'pi pi-fw pi-file-excel', command:()=>{ this.navigateToPage('/participate') }, 
+                label: 'An einem Benchmark teilnehmen', icon: 'pi pi-fw pi-file-excel', command:()=>{ this.navigateToPage(process.env.PUBLIC_URL+'/participate') }, 
             },
             {
-                label: 'Benchmark erstellen', icon: 'pi pi-fw pi-cog', command:()=>{ this.navigateToPage('/create') },
+                label: 'Benchmark erstellen', icon: 'pi pi-fw pi-cog', command:()=>{ this.navigateToPage(process.env.PUBLIC_URL+'/create') },
             },
             {
-                label: 'Architektur', icon: 'pi pi-fw pi-briefcase', command:()=>{this.navigateToPage('/architecture')}
+                label: 'Architektur', icon: 'pi pi-fw pi-briefcase', command:()=>{this.navigateToPage(process.env.PUBLIC_URL+'/architecture')}
             },
             {
-                label: 'Impressum', icon: 'pi pi-fw pi-users', command:()=>{this.navigateToPage('/imprint')}
+                label: 'Impressum', icon: 'pi pi-fw pi-users', command:()=>{this.navigateToPage(process.env.PUBLIC_URL+'/imprint')}
             }
         ]
         return(<MegaMenu  model={items} className="p-mb-4" />)
